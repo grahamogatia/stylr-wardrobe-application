7fe4e37d632c8765d9c75578e2f741e7120fae38
grahamogatia <graham_joshua_ogatia@dlsu.edu.ph>
2024-12-06 01:43:04 +0800

    feat: implement firestore database

--- app/src/main/java/com/example/stylrwardrobeapplication/MainActivity.kt
+++ app/src/main/java/com/example/stylrwardrobeapplication/MainActivity.kt
@@ -2,17 +2,44 @@ package com.example.stylrwardrobeapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.stylrwardrobeapplication.databinding.ActivityHomepageBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomepageBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var user: FirebaseUser
+    private lateinit var db: FirebaseFirestore
+
+    private lateinit var tvUsername: TextView
+    private lateinit var btnLogout: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityHomepageBinding.inflate(layoutInflater)
+        tvUsername = binding.tvUsername
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        user = auth.currentUser!!
+
+        if (user == null) {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            fetchUserData(user.uid)
+        }
+
 
         // Handle BottomNavigationView item clicks
         binding.bottomNavMenu.setOnItemSelectedListener { item ->
@@ -36,6 +63,25 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 else -> false
+            }
+        }
+    }
+
+    private fun fetchUserData(uid: String) {
+        val userRef = db.collection("users").document(uid)
+
+        userRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document: DocumentSnapshot = task.result!!
+                if (document.exists()) {
+                    // Assuming you have a field 'username' in your Firestore document
+                    val username = document.getString("username")
+                    tvUsername.text = username
+                } else {
+                    tvUsername.text = "User!"
+                }
+            } else {
+                tvUsername.text = "Error fetching user data: ${task.exception?.message}"
             }
         }
     }
